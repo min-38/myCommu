92c6548b4e9df72ffbd859abacb48c1e75d62d67
kms <mindino03@gmail.com>
2023-02-01 00:09:27 +0900

    Update - Article Domain Update
    Article 도메인 데이터베이스 접근 로직 접근 코드 추가

--- src/main/java/com/min38/mycommu/domain/Article.java
+++ src/main/java/com/min38/mycommu/domain/Article.java
@@ -1,16 +1,62 @@
 package com.min38.mycommu.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title; // 본문
-    private String content; // 본문
-    private String hashtag; // 해시태그
+    @Setter @Column(nullable = false) private String title; // 본문
+    @Setter @Column(nullable = false, length = 1000) private String content; // 본문
+    @Setter private String hashtag; // 해시태그
 
-    private LocalDateTime createdat; // 작성 날짜
-    private String createdby; // 작성자
-    private LocalDateTime modifiedat; // 수정 날짜
-    private String modifiedby; // 수정자
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdate; // 작성 날짜
+    @CreatedBy @Column(nullable = false, length = 1000) private String createdby; // 작성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedate; // 수정 날짜
+    @LastModifiedBy @Column(nullable = false, length = 1000) private String modifiedby; // 수정자
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
