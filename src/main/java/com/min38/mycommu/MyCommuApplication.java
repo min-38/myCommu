d19f46a04cb529b61e6b3c0b6865adb1472f16de
kms <mindino03@gmail.com>
2023-01-31 22:15:04 +0900

    Initialize Spring Boot Project

---
+++ src/main/java/com/min38/mycommu/MyCommuApplication.java
@@ -0,0 +1,13 @@
+package com.min38.mycommu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCommuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/min38/mycommu/MyCommuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min38.mycommu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCommuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
