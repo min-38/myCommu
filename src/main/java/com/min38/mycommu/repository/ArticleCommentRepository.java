ee79e8a0535060c5ae2768187ffae0539c77a767
kms <mindino03@gmail.com>
2023-02-06 21:26:32 +0900

    Update - data rest 설정
    게시글, 댓글의 json api를 자동으로 restful 하게 만들게끔 설정

--- src/main/java/com/min38/mycommu/repository/ArticleCommentRepository.java
+++ src/main/java/com/min38/mycommu/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.min38.mycommu.repository;
 
 import com.min38.mycommu.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
