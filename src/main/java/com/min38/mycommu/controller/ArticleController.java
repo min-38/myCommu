faa0c507d6f09327e1ecac6747483106126edff4
kms <mindino03@gmail.com>
2023-02-07 22:13:14 +0900

    Add - Article Test
    게스글 관련 뷰 테스트 작성

---
+++ src/main/java/com/min38/mycommu/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package com.min38.mycommu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * /articles/search
+ * /articles/search-hashtag
+ * */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
