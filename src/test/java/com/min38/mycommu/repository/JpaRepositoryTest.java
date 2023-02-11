700f94deed31c7e6f0c0f127333d817113334e1c
kms <mindino03@gmail.com>
2023-02-11 13:57:17 +0900

    Add - Login Controller Test and apply Securiy Setting
    Spring Security가 생성해준 로그인 컨트롤러를 테스트,
    모든 컨트롤러가 Securiy 설정을 부를 수 있도록 import 추가
    
    * 기타 Test 소스 수정

--- src/test/java/com/min38/mycommu/repository/JpaRepositoryTest.java
+++ src/test/java/com/min38/mycommu/repository/JpaRepositoryTest.java
@@ -5,6 +5,7 @@ import com.min38.mycommu.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
@@ -15,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class JpaRepositoryTest {
 
     private ArticleRepository articleRepository;
