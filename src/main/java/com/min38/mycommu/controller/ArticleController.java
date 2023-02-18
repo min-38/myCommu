cad726858c55de6177aeca600ac5e4a2f2f8c091
kms <mindino03@gmail.com>
2023-02-18 16:04:26 +0900

    Update - 게시판 Controller
    게시판 컨트롤러 작업

--- src/main/java/com/min38/mycommu/controller/ArticleController.java
+++ src/main/java/com/min38/mycommu/controller/ArticleController.java
@@ -1,31 +1,46 @@
 package com.min38.mycommu.controller;
 
+import com.min38.mycommu.domain.type.SearchType;
+import com.min38.mycommu.dto.response.ArticleResponse;
+import com.min38.mycommu.dto.response.ArticleWithCommentsResponse;
+import com.min38.mycommu.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-/**
- * /articles/search
- * /articles/search-hashtag
- * */
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(ModelMap map) {
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
         map.addAttribute("articles", List.of());
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 함
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponse());
 
         return "articles/detail";
     }
